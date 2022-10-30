ab79d48cdbec07ab453e99a581f1213079bce694
YunKukPark <a01043340999@gmail.com>
2022-10-30 13:38:12 +0900

    Add: Implement infinity scroll

---
+++ src/Pages/Home/Feeds/FetchMore/FetchMore.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from 'react';
+import './FetchMore.scss';
+
+const FetchMore = ({ loading, page, setPage, limit }) => {
+  const trigger = useRef(null);
+  const observer = new IntersectionObserver(([{ isIntersecting }]) => {
+    if (isIntersecting) setPage(prev => prev + 1);
+  });
+
+  useEffect(() => {
+    observer.observe(trigger.current);
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <div className={`lds-ring ${loading ? 'loading' : ''}`} ref={trigger}>
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+};
+
+export default FetchMore;
--- src/Pages/Home/Feeds/index.jsx
+++ src/Pages/Home/Feeds/index.jsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import FetchMore from './FetchMore';
 import Feed from './Feed/Feed';
 
 const DATA_URL = '/mock/response';
 
+const PAGE_LIMIT = 9;
+
 const Feeds = () => {
+  const [page, setPage] = useState(1);
   const [feedContents, setFeedContents] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`${DATA_URL}/feeds.json`)
+    fetch(`${DATA_URL}/feeds/${page}.json`)
       .then(res => res.json())
-      .then(data => setFeedContents(data));
+      .then(data => setFeedContents([...feedContents, ...data]));
 
     setIsLoading(false);
-  }, []);
+  }, [page]);
 
   if (isLoading) return <p>loading...</p>;
 
@@ -22,6 +26,7 @@ const Feeds = () => {
       {feedContents.map(feed => (
         <Feed key={feed.feedId} feed={feed} />
       ))}
+      {page <= PAGE_LIMIT && <FetchMore page={page} setPage={setPage} />}
     </>
   );
 };
